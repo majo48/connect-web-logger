472b97176e8f096714aece8242f15b75237f9285
majo48 <mart.jonasse@gmail.com>
2020-12-14 21:24:52 +0100

    Added HTML session logic Use selenium for opening the froeling.com pages. Implemented manual login and opened the SYSTEM INFORMATION PAGE.

--- DEPENDENCIES.md
+++ DEPENDENCIES.md
@@ -5,14 +5,13 @@ This application is built and tested with the following dependencies:
 - ***python*** (3.8): programming language
 - ***pip*** (20.3.1): package installer for Python
 - ***setuptools*** (50.0.0): library for packaging Python projects
-- ***requests*** (2.25.0): Python HTTP library
-- ***lxml*** (4.6.2): Python XML and HTML library
-  - certifi (2020.12.5): dependency of request,lxml
-  - chardet (3.0.4): dependency of request,lxml
-  - idna (2.10): dependency of request,lxml
-  - urllib3 (1.26.2: dependency of request,lxml
+- ***selenium*** (3.141.0): framework for software testing
+  - urllib (1,26,2): library for selenium
+- ***phantomjs*** (1.3.0): discontinued headless browser used for automating web page interaction
 
 Developed with:
 
 - ***pycharm*** (Professional 2020.3): Jetbrains Python IDE 
 - dependencies are located in a new virtual environment, stored in the project repository folder ***connect-web-logger/venv*** (excluded in .gitignore)
+- exceptions for MacOS: 
+  - also install geckodriver ($ brew install geckodriver)
--- logger/session.py
+++ logger/session.py
@@ -1,42 +1,60 @@
 """ this file contains code for querying the connect-web registered account
     functions:
-        _login
-        _get_infos
-        _logout
+        _login(...)
+        _get_infos()
+        _logout()
 """
+from logger import local_settings
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class Session:
-    """ open, close a HTML session, scrape key: value pairs from website """
+    """ login to a HTML session, scrape key: value pairs from website and logout """
     def __init__(self, login_url, username, password):
-        self._login(login_url, username, password)
-        self._get_infos()
-        self._logout()
-        pass
-
+        try:
+            self._login(login_url, username, password)
+            self._get_infos()
+            self._logout()
+            pass
+        except Exception as e:
+            print('Error: ' + str(e))
+            pass
+        finally:
+            self._logout()
 
     def _login(self, login_url, username, password):
-        print('login to url: '+login_url)
-
+        print('loging in to url: ' + login_url)
+        # open login page
+        self.driver = webdriver.Firefox()
+        self.driver.get(login_url)
+        # wait for manual login to complete
+        WebDriverWait(self.driver, 120).until(EC.url_changes(login_url))
+        if self.driver.current_url == local_settings.facility_url():
+            # successful login
+            print('successfull login')
+        else:
+            print('Error logging in')
+        pass
 
     def _get_facility_info(self):
         print('facility info')
-
+        self.driver.get(local_settings.facility_info_url())
+        #todo continue here
+        WebDriverWait(self.driver, 120).until(EC.url_changes(local_settings.facility_info_url()))
 
     def _get_boiler_info(self):
         print('boiler info')
 
-
     def _get_heating_info(self):
         print('heating circuit 01 info')
-
 
     def _get_tank_info(self):
         print('DHW tank 01 info')
 
-
     def _get_fead_info(self):
         print('feed system info')
-
 
     def _get_infos(self):
         self._get_facility_info()
@@ -46,6 +64,7 @@ class Session:
         self._get_fead_info()
         pass
 
-
     def _logout(self):
         print('logout')
+        self.driver.quit()
+
