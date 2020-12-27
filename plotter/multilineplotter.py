4c4885e6b9dffb1d2e0f7a12e0d8863293253fc3
majo48 <mart.jonasse@gmail.com>
2020-12-27 12:21:59 +0100

    Change plotter style Use the matplotlib OO style for programming the visualization (still learning).

--- plotter/multilineplotter.py
+++ plotter/multilineplotter.py
@@ -10,20 +10,23 @@ from logger import database
 class Plotter:
     """ plotter implementation """
     def __init__(self, from_date):
+
         db = database.Database()
         # distinct timestamps (x)
         xlist = db.get_timestamps(from_date)
+        xlist = [x[:-3] for x in xlist]  # remove seconds
         # flue temp (y)
         ylist1 = db.get_rows_with(from_date, 'Boiler02')
         # boiler temp (y)
         ylist2 = db.get_rows_with(from_date, 'Boiler01')
 
-        # build plot
-        plt.plot(xlist, ylist1)
-        plt.plot(xlist, ylist2)
-        plt.savefig('plot.png')
-        plt.show()
+        fig, ax = plt.subplots(figsize=(11.6,8.2)) # object oriented IF
+        ax.plot(xlist, ylist1, label='Rauchgas')
+        ax.plot(xlist, ylist2, label='Boiler')
+        ax.legend()
 
+        fig.savefig('plot.png')
+        fig.show()
 
 if __name__ == '__main__':
     print('So sorry, the ' + os.path.basename(__file__) + ' module does not run as a standalone.')
