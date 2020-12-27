1db0a0dd1d894b7f163b65fd71bb285a7c55c5c0
majo48 <mart.jonasse@gmail.com>
2020-12-27 13:49:39 +0100

    Change plotter style Upgraded x-axis to datetime objects (numpy), added labels to x and y axis.

--- plotter/multilineplotter.py
+++ plotter/multilineplotter.py
@@ -4,29 +4,44 @@
 """
 import os, matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 from logger import database
+from datetime import datetime
 
 
 class Plotter:
     """ plotter implementation """
     def __init__(self, from_date):
-
+        """ init plotter """
+        # define which plots
+        ht_plot = {
+            'Boiler02': 'Rauchgas',
+            'Boiler01': 'Boiler'
+        }
+        # open database
         db = database.Database()
-        # distinct timestamps (x)
+        # create x axis as numpy datetime objects
         xlist = db.get_timestamps(from_date)
-        xlist = [x[:-3] for x in xlist]  # remove seconds
-        # flue temp (y)
-        ylist1 = db.get_rows_with(from_date, 'Boiler02')
-        # boiler temp (y)
-        ylist2 = db.get_rows_with(from_date, 'Boiler01')
-
+        xl = []
+        for str in xlist:
+            xl.append(datetime.strptime(str, '%Y-%m-%d %H:%M:%S'))
+        x = np.array(xl)
+        # create & decorate canvas
         fig, ax = plt.subplots(figsize=(11.6,8.2)) # object oriented IF
-        ax.plot(xlist, ylist1, label='Rauchgas')
-        ax.plot(xlist, ylist2, label='Boiler')
+        ax.set_xlabel('Monat-Tag Stunde')
+        ax.set_ylabel('Grad Celcius')
+        # create plots
+        for key, value in ht_plot.items():
+            ylist = db.get_rows_with(from_date, key)
+            if len(ylist) == 0:
+                raise Exception('Invalid key('+key+') for getting a plot')
+            y = np.array(ylist)
+            ax.plot(x, y, label=value)
         ax.legend()
-
+        # create output file
         fig.savefig('plot.png')
         fig.show()
+
 
 if __name__ == '__main__':
     print('So sorry, the ' + os.path.basename(__file__) + ' module does not run as a standalone.')
