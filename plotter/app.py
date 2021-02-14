b6fe0a76861eaabd006d6dd567f1567a8d06867e
majo48 <mart.jonasse@gmail.com>
2021-02-14 10:49:46 +0100

    Added boiler status output Add the Boiler State (number) to the 'rauchgas.png' chart.

--- plotter/app.py
+++ plotter/app.py
@@ -24,7 +24,8 @@ def get_charts():
                 {'dbid': 'Boiler02', 'label': 'Flue Gas [°C]', 'color': 'saddlebrown', 'style': 'solid'},
                 {'dbid': 'Boiler01', 'label': 'Boiler [°C]', 'color': 'orange', 'style': 'solid'},
                 {'dbid': 'Heating02', 'label': 'Flow setpoint [°C]', 'color': 'red', 'style': 'dashed'},
-                {'dbid': 'Tank02', 'label': 'DHW pump [%]', 'color': 'lime', 'style': 'dashed'}
+                {'dbid': 'Tank02', 'label': 'DHW pump [%]', 'color': 'lime', 'style': 'dashed'},
+                {'dbid': 'BoilerNR', 'label': 'Boiler state', 'color': 'grey', 'style': 'dashed'}
             ]
         },
         {
