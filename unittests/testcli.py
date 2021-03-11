be7ede1edeb6f4e9528cdd6c18f119b5885b8ec2
majo48 <mart.jonasse@gmail.com>
2021-03-11 19:23:37 +0100

    Fixed unittest Upgraded the unittest for the log files methode in logger and plotter.

--- unittests/testcli.py
+++ unittests/testcli.py
@@ -7,9 +7,11 @@
 
 import unittest
 import io
+import os
 from contextlib import redirect_stdout
 from logger import app as loggerapp
 from shared import database
+from shared import printlog
 from plotter import app as plotterapp
 
 
@@ -23,35 +25,43 @@ class TestRunCLI(unittest.TestCase):
 
     def test0database(self):
         """ test if database has enough contents for plotting (2 snapshots) """
-        db = database.Database()
+        db = database.Database(print())
         cnt = db.count_logs()
         self.assertGreater(cnt, 2 * 47, "Test environment is not clean!")
 
-    def test1RunClilogger(self):
-        """ run scripting part of logger app, approx. 30 secs """
-        db = database.Database()
+    def test1RunCliLogger(self):
+        """ run scripting part of plotter app """
+        db = database.Database(print())
         cnt = db.count_logs()
         if cnt < + 2 * 47:
             self.skipTest("Test environment is not clean!")
-        f = io.StringIO()
-        with redirect_stdout(f):
-            loggerapp.run('unittest')
-        out = f.getvalue()
-        out = out.splitlines()
-        self.assertEqual(len(out), 10)  # outputs 10 lines
+        loggerapp.run('unittest')
+        my_printer = printlog.PrintLog('testcli.log')
+        foldername = my_printer.get_foldername()
+        filename = foldername + my_printer.get_slash() + 'logger.log'
+        exists = os.path.isfile(filename)
+        self.assertTrue(exists, msg='Missing logger.log file!')
+        if exists:
+            with open(filename) as f:
+                errors = 'Error(' in f.read()
+                self.assertFalse(errors, msg='Error in logger.log file!')
 
     def test2RunCliPlotter(self):
         """ run scripting part of plotter app """
-        db = database.Database()
+        db = database.Database(print())
         cnt = db.count_logs()
         if cnt < + 2 * 47:
             self.skipTest("Test environment is not clean!")
-        f = io.StringIO()
-        with redirect_stdout(f):
-            plotterapp.run()
-        out = f.getvalue()
-        out = out.splitlines()
-        self.assertEqual(len(out), 4)  # outputs 4 lines
+        plotterapp.run()
+        my_printer = printlog.PrintLog('testcli.log')
+        foldername = my_printer.get_foldername()
+        filename = foldername + my_printer.get_slash() + 'plotter.log'
+        exists = os.path.isfile(filename)
+        self.assertTrue(exists, msg='Missing plotter.log file!')
+        if exists:
+            with open(filename) as f:
+                errors = '>>> Error' in f.read()
+                self.assertFalse(errors, msg='Error in plotter.log file!')
 
 
 if __name__ == '__main__':
